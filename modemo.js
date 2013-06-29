d4d4b9d4c818fc31a2214b8488b3393175955395
tiffany <tiff@Tiffany-Browns-MacBook-Pro-2.local>
2013-06-29 11:23:51 -0700

    Adding draft thus far.

---
+++ modemo.js
@@ -0,0 +1,20 @@
+(function(d){
+	var thismany = 2500,
+	btn = d.querySelector('button'),
+	demo = d.querySelector('.demo'),
+	clickhandler = function(){
+		var i = 0, 
+		docfrag = d.createDocumentFragment(), 
+		n = thismany, p,
+		sentence = 'Lorem ipsum dolar sit amet consecutum.';
+		
+		while( i < n ){
+			p = (p !== undefined) ? p = p.cloneNode(false) : p = d.createElement('p');
+			p.textContent = sentence;
+			docfrag.appendChild(p);
+			i++;
+		}		
+		demo.appendChild(docfrag);
+	}
+	btn.addEventListener('click',clickhandler,false);	
+})(document)
